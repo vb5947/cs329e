dbdf8152530afae5be80a54fd67ed2f3d925923a
Fares Fraij <fares@cs.utexas.edu>
2017-02-27 07:39:08 -0600

    uploading additional material

---
+++ examples/Lambdas.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+#pylint: disable = bad-builtin, function-redefined
+
+# ----------
+# Lambdas.py
+# ----------
+
+from functools import reduce
+
+print("Lambdas.py")
+
+def my_function (x, y) :
+    return x + y
+
+bf = my_function
+
+assert my_function(2, 3) == 5
+assert          bf(2, 3) == 5
+
+assert reduce(my_function, [2, 3, 4], 0) == 9
+assert reduce(bf,          [2, 3, 4], 0) == 9
+
+
+
+bf = lambda x, y : x + y
+
+assert (lambda x, y : x + y)(2, 3) == 5
+assert bf(2, 3)                    == 5
+
+assert reduce(lambda x, y : x + y, [2, 3, 4], 0) == 9
+assert reduce(bf,                  [2, 3, 4], 0) == 9
+
+
+
+def my_function () :
+    def f (x, y) :
+        return x + y
+    return f
+
+bf = my_function()
+
+assert my_function()(2, 3) == 5
+assert            bf(2, 3) == 5
+
+assert reduce(my_function(), [2, 3, 4], 0) == 9
+assert reduce(bf,            [2, 3, 4], 0) == 9
+
+
+
+def my_lambda () :
+    return lambda x, y : x + y
+
+bf = my_lambda()
+
+assert my_lambda()(2, 3) == 5
+assert          bf(2, 3) == 5
+
+assert reduce(my_lambda(), [2, 3, 4], 0) == 9
+assert reduce(bf,          [2, 3, 4], 0) == 9
+
+
+
+def my_closure (x) :
+    def f (y) :
+        return x + y
+    return f
+
+uf = my_closure(2)
+
+assert my_closure(2)(3) == 5
+assert            uf(3) == 5
+
+assert list(map(my_closure(2), [2, 3, 4])) == [4, 5, 6]
+assert list(map(uf,            [2, 3, 4])) == [4, 5, 6]
+
+
+
+def my_closure (x) :
+    return lambda y : x + y
+
+uf = my_closure(2)
+
+assert my_closure(2)(3) == 5
+assert            uf(3) == 5
+
+assert list(map(my_closure(2), [2, 3, 4])) == [4, 5, 6]
+assert list(map(uf,            [2, 3, 4])) == [4, 5, 6]
+
+print("Done.")
---
+++ exercises/Range.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+#pylint: disable = too-few-public-methods
+
+# --------
+# Range.py
+# --------
+
+class range_1 :
+    class iterator () :
+        def __init__ (self, b, e) :
+            self.b = b
+            self.e = e
+
+        def __iter__ (self) :
+            return self
+
+        def __next__ (self) :
+            if self.b == self.e :
+                raise StopIteration()
+            v = self.b
+            self.b += 1
+            return v
+
+    def __init__ (self, b, e) :
+        self.b = b
+        self.e = e
+
+    def __iter__ (self) :
+        return range_1.iterator(self.b, self.e)
+
+class range_2 :
+    def __init__ (self, b, e) :
+        self.b = b
+        self.e = e
+
+    def __iter__ (self) :
+        b = self.b
+        e = self.e
+        while b != e :
+            yield b
+            b += 1
